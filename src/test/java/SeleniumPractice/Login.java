7dc4532893b9f1daf2d663f17d07d1cfc2dc710e
Satya Komera <satyakomera@Satyas-MacBook-Air.local>
2025-09-09 19:25:33 +0530

    first commit

---
+++ src/test/java/SeleniumPractice/Login.java
@@ -0,0 +1,32 @@
+package SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	
+	@Test
+	public void invokeurl() throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.makemytrip.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//span[@class='commonModal__close']")).click();
+		//driver.close();
+		
+		driver.findElement(By.id("fromCity")).sendKeys("hy");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//li[@id='react-autowhatever-1-section-0-item-12']")).click();
+		//driver.findElement(By.xpath("//*[contains(.,'Hyderabad Begumpet Airport')]")).click();
+		
+		Thread.sleep(3000);
+		
+		driver.quit();
+		
+		
+	}
+}
